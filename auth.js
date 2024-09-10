23a1e827d687e5aab50c127516fb28d12490782a
YCFProgrammers <139658086+VisteBien@users.noreply.github.com>
2024-09-10 11:12:43 -0500

    fsf

--- auth.js
+++ auth.js
@@ -5,6 +5,7 @@ function onSignIn(googleUser) {
     window.location.href = 'page.html';
 }
 
+
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
